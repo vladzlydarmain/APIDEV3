68fc93d9d1488834b7ed68595348ae3ef757e0ea
Vlad <vladzlydar2008@gmail.com>
2023-12-09 23:37:20 +0200

    modified:   db.js 	modified:   routes/question.js

--- db.js
+++ db.js
@@ -10,6 +10,15 @@ const app = new Client({
 
 app.connect()
 
+function getRandomInt(min, max,callback) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    let number = Math.floor(Math.random() * (max - min + 1)) + min
+    callback(number)
+}
+
 const createTableUser = () => {
     const query = `CREATE TABLE "User"(
         id SERIAL PRIMARY KEY,
@@ -30,7 +39,20 @@ function createTableCategories(){
     app.query(query)
 }
 
-
+function createTableQuestions(){
+    const query = `CREATE TABLE "Questions"(
+        id SERIAL PRIMARY KEY,
+        question VARCHAR(255),
+        correctAnswer VARCHAR(255),
+        incorrectAnswers text[],
+        creator INT,
+        difficulty INT,
+        category INT,
+        creationDate VARCHAR(255), 
+        updateDate VARCHAR(255)
+    );`
+    app.query(query) 
+}
 
 function addUser(username,firstName,lastName,APIKey,isAdmin){
     const query = `INSERT INTO "User"(username, firstName, lastName, APIKey, isAdmin) VALUES('${username}','${firstName}','${lastName}','${APIKey}',${isAdmin});`
@@ -39,7 +61,7 @@ function addUser(username,firstName,lastName,APIKey,isAdmin){
 
 function getUserByApiKey(APIKey, callback){
     const query = `SELECT * FROM "User" WHERE APIKey = '${APIKey}'`
-    app.query(query,(err,res)=>{
+    app.query(query,(_err,res)=>{
         // console.log(err)
         // console.log(res)
         callback(res.rows)
@@ -57,7 +79,7 @@ function getUserById(id, callback){
 
 function getAllUsers(callback){
     const query = `SELECT * FROM "User"`
-    app.query(query, (err,res)=>{
+    app.query(query, (_err,res)=>{
         console.log(res)
         callback(res.rows)
     })
@@ -87,7 +109,7 @@ function updateUser(id,username,firstName,lastName,isAdmin){
             isAdmin = res[0].isadmin
         }
         const query = `UPDATE "User" SET username = '${username}', firstName = '${firstName}', lastName = '${lastName}', isAdmin = ${isAdmin} WHERE id = ${id}`
-        app.query(query,(err,res)=>{
+        app.query(query,(err,_res)=>{
             console.log(err)
         })
     })
@@ -104,14 +126,14 @@ function getAllCategories(callback){
 
 function addCategory(name,callback){
     const query = `INSERT INTO "Categories"(name) VALUES('${name}') RETURNING id`
-    app.query(query,(err,res)=>{
+    app.query(query,(_err,res)=>{
         callback(res.rows)
     })
 } 
  
 function deleteCategory(id){
     const query = `DELETE FROM "Categories" WHERE id = ${id}`
-    app.query(query,(err,res)=>{
+    app.query(query,(err,_res)=>{
         console.log(err) 
     }) 
 } 
@@ -127,31 +149,91 @@ function getCategoryById(id,callback){
 function updateCategory(id, name){
     const query = `UPDATE "Categories" SET name = '${name}' WHERE id = ${id}`
     app.query(query)
-}
-
-function createTableQuestions(){
-    const query = `CREATE TABLE "Questions"(
-        id SERIAL PRIMARY KEY,
-        question VARCHAR(255),
-        correctAnswer VARCHAR(255),
-        incorrectAnswers text[],
-        creator INT,
-        category INT,
-        creationDate VARCHAR(255), 
-        updateDate VARCHAR(255)
-    );`
-    app.query(query) 
 }   
    
-function addQuestion(question, correctAnswer, incorrectAnswers, creator, category){
+function addQuestion(question, correctAnswer, incorrectAnswers, creator,difficulty, category,callback){
     const date = new Date()
     const updateDate = new Date(date.getUTCFullYear(),date.getUTCMonth(),date.getUTCDate(),date.getHours(),date.getMinutes(),date.getSeconds(),date.getMilliseconds()).toUTCString() 
     const creationDate = new Date(date.getUTCFullYear(),date.getUTCMonth(),date.getUTCDate(),date.getHours(),date.getMinutes(),date.getSeconds(),date.getMilliseconds()).toUTCString()
-    const query = `INSERT INTO "Questions"(question, correctAnswer, incorrectAnswers, creator, category, creationDate, updateDate) VALUES('${question}', '${correctAnswer}', '{${incorrectAnswers[0]},${incorrectAnswers[1]},${incorrectAnswers[2]}}', '${creator}', '${category}', '${creationDate}', '${updateDate}')`
+    const query = `INSERT INTO "Questions"(question, correctAnswer, incorrectAnswers, creator,difficulty, category, creationDate, updateDate) VALUES('${question}', '${correctAnswer}', '{${incorrectAnswers[0]},${incorrectAnswers[1]},${incorrectAnswers[2]}}', '${creator}','${difficulty}','${category}', '${creationDate}', '${updateDate}') RETURNING id`
     app.query(query,(err,res)=>{
         console.log(err)
+        // console.log(res)
+        callback(res.rows) 
     })    
 } 
+
+function getRandomQuestions(limit, category, difficulty, callback){
+    const array = []
+    console.log(category)
+    console.log(difficulty)
+    let query = `SELECT * FROM "Questions" WHERE category = '${category}' AND difficulty = '${difficulty}'`
+    if(difficulty == 0 && category == 0){
+        query = `SELECT * FROM "Questions"`
+    } else if(difficulty == 0){
+        query = `SELECT * FROM "Questions" WHERE category = '${category}'`
+    } else if(category == 0){
+        query = `SELECT * FROM "Questions" WHERE difficulty = '${difficulty}'`
+    }
+    app.query(query,(err, res) => {
+        console.log(err)
+        console.log(1)
+        console.log(res.rows)
+        for(let i = 0; i < limit;i++){
+            getRandomInt(0,res.rows.length-1,(id)=>{
+                array.push(res.rows[id])
+            })
+        }
+        callback(array)
+    })
+}
+
+function getQuestionById(id, callback){
+    const query = `SELECT * FROM "Questions" WHERE id = '${id}'`
+    app.query(query, (err, res) => {
+        console.log(err)
+        callback(res.rows)
+    })
+}
+
+function deleteQuestionById(id){
+    const query = `DELETE FROM "Questions" WHERE id = '${id}'`
+    app.query(query)
+}
+
+function updateQuestion(id, question, correctAnswer, incorrectAnswers, difficulty, category){
+    const date = new Date()
+    const updateDate = new Date(date.getUTCFullYear(),date.getUTCMonth(),date.getUTCDate(),date.getHours(),date.getMinutes(),date.getSeconds(),date.getMilliseconds()).toUTCString() 
+    getQuestionById(id,(resDB)=>{
+        if(!question){
+            question = resDB[0].question
+        }
+        if(!correctAnswer){
+            correctAnswer = resDB[0].correctanswer
+        }
+        if(!incorrectAnswers){
+            incorrectAnswers = resDB[0].incorrectanswers
+        }
+        console.log(incorrectAnswers.length)
+        if(incorrectAnswers.length < 3 && incorrectAnswers.length > 0){
+            for(let i in resDB[0].incorrectanswers){
+                if(!incorrectAnswers[i]){
+                    incorrectAnswers.push(resDB[0].incorrectanswers[i])
+                }
+            }
+        }
+        if(!difficulty){
+            difficulty = resDB[0].difficulty
+        }
+        if(!category){
+            category = resDB[0].category
+        }
+        const query = `UPDATE "Questions" SET question = '${question}', correctAnswer = '${correctAnswer}', incorrectAnswers = '{${incorrectAnswers[0]},${incorrectAnswers[1]},${incorrectAnswers[2]}}', difficulty = '${difficulty}', category = '${category}', updateDate = '${updateDate}' WHERE id = ${id}`
+        app.query(query,(err,res)=>{
+            console.log(err)
+        })
+    })
+}
  
 // createTableUser()
 // createTableCategories()    
@@ -169,5 +251,9 @@ module.exports = {
     deleteCategory: deleteCategory,
     getCategoryById:getCategoryById,
     updateCategory: updateCategory,
-    addQuestion:addQuestion
+    addQuestion:addQuestion,
+    getRandomQuestions:getRandomQuestions,
+    getQuestionById:getQuestionById,
+    deleteQuestionById:deleteQuestionById,
+    updateQuestion:updateQuestion
 }
--- routes/question.js
+++ routes/question.js
@@ -13,6 +13,7 @@ router.post('/', (req, res) => {
         const incorrectAnswers = req.body.incorrectAnswers
         const creator = user[0].id
         const category = req.body.category
+        let difficulty = req.body.difficulty
         if(!question){
             return res.json({
                 code:400,
@@ -60,19 +61,177 @@ router.post('/', (req, res) => {
                 code:400,  
                 message: "Category must be int"
             }).status(400) 
-        }  
+        } 
+        if(Number(difficulty) > 3 || Number(difficulty) <= 0){
+            return res.json({
+                code:400,  
+                message: "Difficulty must be less or equals 3 and more than 0" 
+            }).status(400) 
+        }
+        if(!difficulty){
+            difficulty = 0
+        }
         db.getCategoryById(category,(resDB)=>{
             if(resDB.length < 1){
                 return res.json({
                     code:404,
-                    message: "Category does not exist"
+                    message: "Category does not exist"  
                 }).status(404)
             }
-            db.addQuestion(question, correctAnswer, incorrectAnswers, creator, category)
-            res.json({
-                code:201,
-                message: "Question has been created"
-            }).status(201) 
+            db.addQuestion(question, correctAnswer, incorrectAnswers, creator,difficulty, category,(id)=>{
+                console.log(id)
+                return res.json({
+                    code:201,
+                    message: `Question has been created with id - ${id[0].id}`
+                }).status(201) 
+            })  
+        })
+    })
+})
+
+router.get('/', (req, res) => {
+    test.checkAPIKey(req, res, (user) => {
+        const limit = req.query.limit
+        let category = req.query.category
+        let difficulty = req.query.difficulty
+        if(limit == undefined){
+            return res.json({
+                code: 400,
+                message: "Limit must exist"
+            }).status(400)
+        }
+        if(Number(difficulty) > 3 || Number(difficulty) <= 0){
+            return res.json({
+                code:400,  
+                message: "Difficulty must be less or equals 3 and more than 0" 
+            }).status(400) 
+        }
+        if(!difficulty){
+            difficulty = 0
+        }
+        if(category == undefined){
+            category = 0
+        }
+        db.getCategoryById(category,(resDB)=>{
+            if(resDB.length < 1){
+                if(category != 0){
+                    return res.json({
+                        code:404,
+                        message: "Category does not exist"  
+                    }).status(404)
+                } 
+            }
+            db.getRandomQuestions(limit,category,difficulty,(set)=>{
+                if(set.length > 0 && set[0] != null){
+                    return res.json(set)
+                } else {
+                    return res.json({
+                        code:404,
+                        message: "Questions like this don`t exist"  
+                    }).status(404)
+                }
+            })
+        })
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    test.checkAPIKey(req,res,(user)=>{
+        db.getQuestionById(req.params.id,(resDB)=>{
+            if(resDB.length <= 0){
+                return res.json({
+                    code:404,
+                    message: "Question does not exist"  
+                }).status(404)
+            }
+            return res.json(resDB)
+        })
+    })
+})
+
+router.delete('/:id',(req,res)=>{
+    test.checkAPIKey(req,res,(user)=>{
+        db.getQuestionById(req.params.id,(question)=>{
+            if(question.length <= 0){
+                return res.json({
+                    code:404,
+                    message:"Question does not exist"
+                }).status(404)
+            }
+            if (user[0].isadmin || question[0].creator == user[0].id){  
+                db.deleteQuestionById(req.params.id)
+                return res.json({
+                    code:200,
+                    message:"Question has been deleted successfully"
+                }).status(200)
+            } else {
+                return res.json({
+                    code:403,
+                    message:"You don't have access"
+                }).status(403)           
+            }
+        })
+    })
+})
+
+router.put('/:id',(req,res)=>{
+    test.checkAPIKey(req,res,(user)=>{
+        const id = req.params.id 
+        const question = req.body.question
+        const correctAnswer = req.body.correctAnswer
+        const incorrectAnswers = req.body.incorrectAnswers
+        const creator = user[0].id
+        let category = req.body.category
+        let difficulty = req.body.difficulty
+        if(question){
+            if(question.length < 7){
+                return res.json({
+                    code:400,
+                    message: "Question must be at least 7 symbols long"
+                }).status(400)
+            }
+        }
+        if(incorrectAnswers){
+            if(typeof incorrectAnswers != "object"){
+                return res.json({ 
+                    code:400,
+                    message: "Incorrect answers must be Array with 3 str elements"
+                }).status(400)
+            }
+        }
+        if(category){
+            if(!Number(category) && category != undefined){  
+                return res.json({
+                    code:400,  
+                    message: "Category must be int"
+                }).status(400) 
+            } 
+        } else {
+            category = 0
+        }
+        if(difficulty){
+            if(Number(difficulty) > 3 || Number(difficulty) <= 0){
+                return res.json({
+                    code:400,  
+                    message: "Difficulty must be less or equals 3 and more than 0" 
+                }).status(400) 
+            }
+        }
+        db.getCategoryById(category,(resDB)=>{
+            if(resDB.length < 1){
+                if(category != 0){
+                    return res.json({
+                        code:404,
+                        message: "Category does not exist"  
+                    }).status(404)
+                } 
+            }
+            db.updateQuestion(id,question, correctAnswer, incorrectAnswers,difficulty, category)
+            return res.json({
+                code:200,
+                message: `Question has been updated`
+            }).status(200) 
+             
         })
     })
 })
